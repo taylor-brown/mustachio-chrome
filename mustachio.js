744f1bd5828658769360794d692d2c363fc03bc4
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2011-08-12 17:59:43 +0100

    reverting back to the original api

--- mustachio.js
+++ mustachio.js
@@ -2,7 +2,7 @@ $(function() {
   $('img').each(function(i,e) {
     var originalImage = e.src;
     if (!originalImage) return;
-    var newImage = "http://mustachy.herokuapp.com/" + originalImage;
+    var newImage = "http://mustachio.herokuapp.com/?src=" + originalImage;
     $.get(newImage, function() {
       $(e).attr('src', newImage);
     })
