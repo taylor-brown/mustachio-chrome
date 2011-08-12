3077cc81cf85892f7ec89a39e93b9513b86d17bb
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2011-08-12 20:11:56 +0100

    enable/disable button

--- mustachio.js
+++ mustachio.js
@@ -1,10 +1,18 @@
 $(function() {
   $('img').each(function(i,e) {
-    var originalImage = e.src;
-    if (!originalImage) return;
-    var newImage = "http://mustachy.heroku.com/?src=" + originalImage;
-    $.get(newImage, function() {
-      $(e).attr('src', newImage);
-    })
-  })
+    chrome.extension.sendRequest({method: "isEnabled"}, function(response) {
+      if (response.enabled == 'true') {
+        console.log('enabled');
+      } else {
+        console.log("disabled");
+        return;
+      }
+      var originalImage = e.src;
+      if (!originalImage) return;
+      var newImage = "http://mustachy.heroku.com/?src=" + originalImage;
+      $.get(newImage, function() {
+        $(e).attr('src', newImage);
+      })      
+    });    
+  });
 })
