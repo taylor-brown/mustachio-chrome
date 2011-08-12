b8f9d243dca81c0c4cbcdd5652f0d6e3b13f2eea
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2011-08-12 16:29:11 +0100

    MVP

---
+++ mustachio.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('img').each(function(i,e) {
+    var originalImage = e.src;
+    if (!originalImage) return;    
+    var newImage = "http://mustachio.heroku.com/?src=" + originalImage;
+    $.get(newImage, function() {
+      $(e).attr('src', newImage);
+    })
+  })
+})
