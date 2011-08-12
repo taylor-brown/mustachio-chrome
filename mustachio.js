b9786e884c70010be0cb800496f277808c8109c5
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2011-08-12 18:37:36 +0100

    switch to our api on the bamboo stack

--- mustachio.js
+++ mustachio.js
@@ -2,7 +2,7 @@ $(function() {
   $('img').each(function(i,e) {
     var originalImage = e.src;
     if (!originalImage) return;
-    var newImage = "http://mustachio.heroku.com/?src=" + originalImage;
+    var newImage = "http://mustachy.heroku.com/?src=" + originalImage;
     $.get(newImage, function() {
       $(e).attr('src', newImage);
     })
