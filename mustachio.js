02c15cc17769578377b6337a0228cf1bb9f7cab0
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2011-08-12 18:01:41 +0100

    fixed the url

--- mustachio.js
+++ mustachio.js
@@ -2,7 +2,7 @@ $(function() {
   $('img').each(function(i,e) {
     var originalImage = e.src;
     if (!originalImage) return;
-    var newImage = "http://mustachio.herokuapp.com/?src=" + originalImage;
+    var newImage = "http://mustachio.heroku.com/?src=" + originalImage;
     $.get(newImage, function() {
       $(e).attr('src', newImage);
     })
